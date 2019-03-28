ae862ee59673e821ee73292e5bea45b7d4846441
Oliver Stenkilde <hello@stenkil.de>
2019-03-28 16:34:24 +0100

    Added tabBar navigation

--- App.tsx
+++ App.tsx
@@ -1,32 +1,68 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { createStackNavigator, createBottomTabNavigator, createAppContainer } from "react-navigation";
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+Icon.loadFont();
 
 import HomeScreen from './Screens/HomeScreen';
 import ContactScreen from './Screens/ContactScreen';
 
-const AppNavigator = createStackNavigator(
+// const AppNavigator = createStackNavigator(
+//   {
+//     Home: HomeScreen,
+//     Contact: ContactScreen
+//   },{
+//     initialRouteName: "Home",
+//     defaultNavigationOptions: {
+//       headerStyle: {
+//         backgroundColor: '#881AE8',
+//       },
+//       headerTintColor: '#fff',
+//       headerTitleStyle: {
+//         fontWeight: 'bold',
+//       },
+//     },
+//   },
+// );
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  // Details: DetailsScreen,
+});
+
+const MoreStack = createStackNavigator({
+  Contact: ContactScreen,
+  // Details: DetailsScreen,
+});
+
+const TabNavigator = createBottomTabNavigator(
   {
-    Home: HomeScreen,
-    Contact: ContactScreen
-  },{
-    initialRouteName: "Home",
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: '#881AE8',
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
-    },
+    Home: HomeStack,
+    Stuff: MoreStack,
   },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `home`;
+          // Sometimes we want to add badges to some icons. 
+          // You can check the implementation below.
+        } else if (routeName === 'Stuff') {
+          iconName = `ellipsis-h`;
+        }
+
+        // You can return any component that you like here!
+        return <Icon name={iconName} size={30} color="#881AE8" />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#881AE8',
+      inactiveTintColor: 'gray',
+    },
+  }
 );
 
-const AppContainer = createAppContainer(AppNavigator);
-
-export default class App extends React.Component {
-  render() {
-    return <AppContainer />;
-  }
-}
+export default createAppContainer(TabNavigator);
