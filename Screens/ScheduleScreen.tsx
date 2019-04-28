f51f6836b84a16c83c36e5c95c735642d3e7463e
Oliver Munkebo <o.munkebo@eslgaming.com>
2019-04-28 22:43:47 +0200

    Updated screen for Horaro

--- Screens/ScheduleScreen.tsx
+++ Screens/ScheduleScreen.tsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {StyleSheet, Button, Text, View} from 'react-native';
 import ScheduleList from '../Components/Schedule/ScheduleList';
 
+import {LoadHoraro} from '../Services/ScheduleService';
+
 export default class ScheduleScreen extends Component {
     static navigationOptions = {
         title: 'Schedule',
@@ -17,20 +19,12 @@ export default class ScheduleScreen extends Component {
     }
 
     componentWillMount() {
-        setTimeout(() => {
+        LoadHoraro().then((response) => {
             this.setState({
-                runs: [{
-                    id: "1",
-                    runnerName: "TMR",
-                    game: "Cuphead",
-                    category: "All Bosses, Pacifist",
-                    startDate: "16th April 2019"
-                }],
+                runs: response,
                 loading: false
-            });
-
-            console.log(this.state);
-        }, 1000)
+            })
+        })
     }
 
     render() {
