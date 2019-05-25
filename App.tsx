4ee8153f102ac3d35459f4b515b6ea9e565e2b47
Oliver Munkebo <o.munkebo@eslgaming.com>
2019-05-25 19:06:27 +0200

    Changed name of themes

--- App.tsx
+++ App.tsx
@@ -3,7 +3,9 @@ import {createAppContainer} from 'react-navigation';
 import {TabNavigator} from './Components/Navigation/MainNavigator';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-export const ThemeContext = React.createContext<{theme: 'light' | 'dark'}>({theme: 'light'});
+export const ThemeContext = React.createContext<{theme: 'default' | 'summer' | 'winter'}>({
+    theme: 'default',
+});
 
 Icon.loadFont();
 
