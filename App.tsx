1469bc1e32071abbf31047c95d519d6cdd649c22
Oliver Munkebo <o.munkebo@eslgaming.com>
2019-05-25 19:01:51 +0200

    Split navigation out to its own component

--- App.tsx
+++ App.tsx
@@ -1,85 +1,11 @@
 import React from 'react';
-import {createStackNavigator, createBottomTabNavigator, createAppContainer} from 'react-navigation';
+import {createAppContainer} from 'react-navigation';
+import {TabNavigator} from './Components/Navigation/MainNavigator';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import HomeScreen from './Screens/HomeScreen';
-import ContactScreen from './Screens/ContactScreen';
-import AnnouncementsScreen from './Screens/AnnouncementsScreen';
-import ScheduleScreen from './Screens/ScheduleScreen';
-import NotificationScreen from './Screens/NotificationScreen';
-import {ThemedBottomTabBar} from './Components/Navigation/ThemedTabBar';
-import {BottomTabBarProps} from 'react-navigation';
 
-const ThemeContext = React.createContext<{theme: 'light' | 'dark'}>({theme: 'light'});
+export const ThemeContext = React.createContext<{theme: 'light' | 'dark'}>({theme: 'light'});
 
 Icon.loadFont();
-
-const HomeStack = createStackNavigator({
-    Home: HomeScreen,
-    // Details: DetailsScreen,
-});
-
-const ScheduleStack = createStackNavigator({
-    Schedule: ScheduleScreen,
-});
-
-const NotificationStack = createStackNavigator({
-    Notification: NotificationScreen,
-});
-
-const AnnouncementsStack = createStackNavigator({
-    Announcements: AnnouncementsScreen,
-});
-
-const MoreStack = createStackNavigator({
-    Contact: ContactScreen,
-    // Details: DetailsScreen,
-});
-
-const WrappedTabBar = (navigation: BottomTabBarProps) => {
-    return (
-        <ThemeContext.Consumer>
-            {({theme}) => <ThemedBottomTabBar navigation={navigation} theme={theme} />}
-        </ThemeContext.Consumer>
-    );
-};
-
-const TabNavigator = createBottomTabNavigator(
-    {
-        Notification: NotificationStack,
-        Schedule: ScheduleStack,
-        Home: HomeStack,
-        Announcements: AnnouncementsStack,
-        More: MoreStack,
-    },
-    {
-        initialRouteName: 'Home',
-        tabBarComponent: WrappedTabBar,
-        defaultNavigationOptions: ({navigation}) => ({
-            tabBarIcon: ({tintColor}) => {
-                const {routeName} = navigation.state;
-
-                const icons: {[x: string]: string} = {
-                    Home: 'home',
-                    Notification: 'bell',
-                    Schedule: 'calendar',
-                    Announcements: 'comments',
-                };
-
-                const iconName = icons[routeName] || 'ellipsis-h';
-
-                return <Icon name={iconName} size={20} color={tintColor || undefined} />;
-            },
-        }),
-        tabBarOptions: {
-            activeTintColor: '#881AE8',
-            inactiveTintColor: 'gray',
-            labelStyle: {
-                marginTop: 0,
-                marginBottom: 5,
-            },
-        },
-    },
-);
 
 const Navigation = createAppContainer(TabNavigator);
 
