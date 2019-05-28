e6a957846867e959b8ba9ecbd6e55d9c000297b8
Oliver Munkebo <o.munkebo@eslgaming.com>
2019-05-28 22:36:51 +0200

    Removed unused React native stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {TabNavigator} from './Components/Navigation/MainNavigator';
 import Icon from 'react-native-vector-icons/FontAwesome';
