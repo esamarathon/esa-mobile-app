5484884c3ed483a8f75689ac4ffc324a0831706c
Oliver Munkebo <o.munkebo@eslgaming.com>
2019-05-30 18:17:04 +0200

    Added basic push stuff.

--- android/app/src/main/java/com/esamobile/MainApplication.java
+++ android/app/src/main/java/com/esamobile/MainApplication.java
@@ -3,12 +3,16 @@ package com.esamobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +29,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseMessagingPackage(),
+            new RNFirebaseNotificationsPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage()
       );
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import {Alert} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {TabNavigator} from './Components/Navigation/MainNavigator';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {AsyncStorage} from 'react-native';
+import firebase from 'react-native-firebase';
 
 export const ThemeContext = React.createContext<{theme: 'default' | 'summer' | 'winter'}>({
     theme: 'default',
@@ -12,6 +15,92 @@ Icon.loadFont();
 const Navigation = createAppContainer(TabNavigator);
 
 export default class AppContainer extends React.Component {
+    async componentDidMount() {
+        this.checkPermission();
+        this.createNotificationListeners();
+    }
+
+    componentWillUnmount() {
+        this.notificationListener();
+        this.notificationOpenedListener();
+    }
+
+    async createNotificationListeners() {
+        /*
+         * Triggered when a particular notification has been received in foreground
+         * */
+        this.notificationListener = firebase.notifications().onNotification((notification) => {
+            const {title, body} = notification;
+            this.showAlert(title, body);
+        });
+
+        /*
+         * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+         * */
+        this.notificationOpenedListener = firebase
+            .notifications()
+            .onNotificationOpened((notificationOpen) => {
+                const {title, body} = notificationOpen.notification;
+                this.showAlert(title, body);
+            });
+
+        /*
+         * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+         * */
+        const notificationOpen = await firebase.notifications().getInitialNotification();
+        if (notificationOpen) {
+            const {title, body} = notificationOpen.notification;
+            this.showAlert(title, body);
+        }
+        /*
+         * Triggered for data only payload in foreground
+         * */
+        this.messageListener = firebase.messaging().onMessage((message) => {
+            //process data message
+            console.log(JSON.stringify(message));
+        });
+    }
+
+    showAlert(title, body) {
+        Alert.alert(title, body, [{text: 'OK', onPress: () => console.log('OK Pressed')}], {
+            cancelable: false,
+        });
+    }
+
+    //1
+    async checkPermission() {
+        const enabled = await firebase.messaging().hasPermission();
+        if (enabled) {
+            this.getToken();
+        } else {
+            this.requestPermission();
+        }
+    }
+
+    //3
+    async getToken() {
+        let fcmToken = await AsyncStorage.getItem('fcmToken');
+        if (!fcmToken) {
+            fcmToken = await firebase.messaging().getToken();
+            if (fcmToken) {
+                // user has a device token
+                await AsyncStorage.setItem('fcmToken', fcmToken);
+            }
+        }
+    }
+
+    //2
+    async requestPermission() {
+        try {
+            await firebase.messaging().requestPermission();
+            // User has authorised
+            this.getToken();
+        } catch (error) {
+            // User has rejected permissions
+            console.log('permission rejected');
+        }
+    }
+
     render() {
         return (
             <ThemeContext.Provider value={{theme: 'default'}}>
