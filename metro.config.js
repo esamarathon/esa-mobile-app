7e525601c90a81e14659bf2273e452cd33acebe7
Christian Ruigrok <christian@ruigrok.info>
2019-07-23 13:41:20 +0200

    cleanup code, fix tests and offline svgs

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,26 @@
+const {getDefaultConfig} = require('metro-config');
 /**
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
  */
 
-module.exports = {
-    transformer: {
-        getTransformOptions: async () => ({
-            transform: {
-                experimentalImportSupport: false,
-                inlineRequires: false,
-            },
-        }),
-    },
-};
+module.exports = (async () => {
+    const {
+        resolver: {sourceExts, assetExts},
+    } = await getDefaultConfig();
+    return {
+        transformer: {
+            babelTransformerPath: require.resolve('react-native-svg-transformer'),
+            getTransformOptions: async () => ({
+                transform: {
+                    experimentalImportSupport: false,
+                    inlineRequires: false,
+                },
+            }),
+        },
+        resolver: {
+            assetExts: assetExts.filter((ext) => ext !== 'svg'),
+            sourceExts: [...sourceExts, 'svg'],
+        },
+    };
+})();
