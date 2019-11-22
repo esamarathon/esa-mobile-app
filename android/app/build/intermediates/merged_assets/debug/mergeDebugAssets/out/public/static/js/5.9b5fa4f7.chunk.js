a1cd4dc19da4431733690b19716da7b3cc28159a
Oliver Munkebo <o.munkebo@eslgaming.com>
2019-11-22 18:55:32 +0100

    Removed android and added gitignore for it

--- android/app/build/intermediates/merged_assets/debug/mergeDebugAssets/out/public/static/js/5.9b5fa4f7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpesa-app"]=this["webpackJsonpesa-app"]||[]).push([[5],{201:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(32),i=(e(6),function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.g)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var a=i.closest("ion-content");a&&a.componentOnReady().then((function(){Object(o.m)((function(){return a.scrollToTop(300)}))}))}}))}))})}}]);
-//# sourceMappingURL=5.9b5fa4f7.chunk.js.map
