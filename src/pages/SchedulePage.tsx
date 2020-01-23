45edd104a9a63cbe1f954d76107c1223baa325a1
Oliver Munkebo <o.munkebo@eslgaming.com>
2020-01-23 18:51:20 +0100

    Added updated schedule page and headers to pages

--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -1,12 +1,81 @@
 import React from 'react';
-import {IonPage} from '@ionic/react';
+import {IonContent, IonPage, IonSpinner} from '@ionic/react';
 import {RouteComponentProps} from 'react-router';
 import Toolbar from '../components/Toolbar';
+import {StyledHeader, StyledHeaderWrapper} from '../components/common/HeaderBar';
+import styled from 'styled-components';
+import useSWR from 'swr';
+import {LoadHoraro} from '../services/ScheduleService';
+import {IEvent} from '../services/EventService';
+import ScheduleCard from '../components/ScheduleCard';
 
-function SchedulePage(_: RouteComponentProps) {
+const Content = styled(IonContent)`
+  background-color: var(--ion-background);
+`;
+
+const DayScroller = styled.ul`
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-y: scroll;
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const ScrollItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 10px;
+`;
+
+const ScrollBorder = styled.span`
+    width: 130%;
+    border-radius: 3px;
+    height: 3px;
+    background #fff;
+    margin-top: 4px;
+`;
+
+const ScheduleList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  margin: 60px 0 0;
+  padding: 0 20px 15px;
+  overflow-x: scroll;
+`;
+
+interface IProps {
+  event: IEvent;
+}
+
+function SchedulePage({event}: IProps & RouteComponentProps) {
+  const {data, isValidating} = useSWR(event.meta.horaro, LoadHoraro);
+
   return (
     <IonPage>
-      <Toolbar>Schedule</Toolbar>
+      <StyledHeaderWrapper>
+        <StyledHeader>
+          <Toolbar opaque>Schedule</Toolbar>
+          <DayScroller>
+            <ScrollItem>
+              <span>Mon</span>
+              <ScrollBorder />
+            </ScrollItem>
+          </DayScroller>
+        </StyledHeader>
+      </StyledHeaderWrapper>
+      <Content>
+        {!data && isValidating ? (
+          <IonSpinner />
+        ) : (
+          <ScheduleList>
+            {(data ? data.data : []).map((run) => (
+              <ScheduleCard key={run.scheduled + (run.players.join('-') || '')} run={run} />
+            ))}
+          </ScheduleList>
+        )}
+      </Content>
     </IonPage>
   );
 }
