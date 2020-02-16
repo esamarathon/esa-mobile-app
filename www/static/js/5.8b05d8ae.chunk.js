b0f82417fd0d8928059725ae51ccb0e9d7d9e168
Oliver Munkebo <o.munkebo@eslgaming.com>
2020-02-16 21:55:50 +0100

    Remove www

--- www/static/js/5.8b05d8ae.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpesa-app"]=this["webpackJsonpesa-app"]||[]).push([[5],{180:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(34),i=(e(15),function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.g)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var a=i.closest("ion-content");a&&a.componentOnReady().then((function(){Object(o.m)((function(){return a.scrollToTop(300)}))}))}}))}))})}}]);
-//# sourceMappingURL=5.8b05d8ae.chunk.js.map
