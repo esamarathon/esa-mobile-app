5e455dde861f17b41fb1be1d79b1e2514c74c214
Oliver Munkebo <o.munkebo@eslgaming.com>
2020-06-27 15:13:08 +0200

    Fixed localStorage issue

--- src/providers/PushProvider.ts
+++ src/providers/PushProvider.ts
@@ -10,12 +10,20 @@ export function cancelNotification(runId: number) {
 }
 
 export async function scheduleNotification(options: any) {
+  if (localStorage.getItem('ESA@notificationId')) {
+    notificationId = parseInt(JSON.parse(localStorage.getItem('ESA@notificationId') as string));
+    notificationId++;
+  } else {
+    notificationId++;
+    localStorage.setItem('ESA@notificationId', JSON.stringify(notificationId));
+  }
+
   return LocalNotifications.schedule({
     notifications: [
       {
         title: options.title,
         body: options.body,
-        id: notificationId++,
+        id: notificationId,
         schedule: {at: options.scheduled},
       },
     ],
